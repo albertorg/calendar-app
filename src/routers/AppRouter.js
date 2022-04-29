23e3e430d6709da7a75cbb0b79d2d0f002328adf
Alberto Rodriguez <ticoc911@gmail.com>
2022-04-29 23:38:43 +0200

    Start Routes

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { LoginScreen } from '../components/auth/LoginScreen'
+import { CalendarScreen } from '../components/calendar/CalendarScreen'
+
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+
+        <Routes>
+            <Route path='/' element={<CalendarScreen />}/>
+            <Route path='/login' element={<LoginScreen />}/>
+            <Route patch='/*' element={<CalendarScreen />}/>
+        </Routes>
+    
+    </BrowserRouter>
+  )
+}
